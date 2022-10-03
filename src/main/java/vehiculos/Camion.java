a5c9bebc0475b1a8d48ed4ccaa0bfaa52b3b03f7
StepXo <66577156+StepXo@users.noreply.github.com>
2022-10-03 01:25:39 -0500

    una maldita X mayuscula :P

--- src/main/java/vehiculos/Camion.java
+++ src/main/java/vehiculos/Camion.java
@@ -5,7 +5,7 @@ public class Camion extends Vehiculo{
     static int camiones=0;
 
     public Camion(String placa, String nombre, int precio, int peso,Fabricante fabricante, int ejes){
-        super(placa,2,80,nombre,precio,peso,"4x2",fabricante);
+        super(placa,2,80,nombre,precio,peso,"4X2",fabricante);
         this.ejes=ejes;
         camiones++;
     }
